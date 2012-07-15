c553a41c0fc05e7d02f59c5f771d9b9312d900d6
Alonso Vega <lavb91@gmail.com>
2012-07-14 18:39:24 -0600

    Commit general xD

--- js/ai.js
+++ js/ai.js
@@ -1,3 +1,17 @@
-function func() {
-alert('hola');
+function validMoves(board,x,y) {
+	value = board[x][y];
+	if (value != 0) {
+		var xlimit = board.length, ylimit = board[0].length;
+		var result = new Array();
+		if (board[x+value][y+1]==0) {
+			result.push(new Array(x+value,y+1));
+		}
+		if (board[x+value][y-1]==0) {
+			result.push(new Array(x+value,y-1));
+		}
+		return result;
+	} else {
+		// No token in selected cell
+		return false;
+	}
 }
--- js/checker.js
+++ js/checker.js
@@ -1,6 +1,7 @@
 var x = $(document);
 var checker;
 var actualPlayer = 1;
+var actualToken;
 var player1 = "img/lemon.png";
 var player2 = "img/grape.png";
 x.ready(initialize);
@@ -31,7 +32,7 @@ function generate(x, y) {
 		for (var j = 0; j < y; j++) {
 
 			// Set value at this position
-			if ((j+i)%2==1) {
+			if ((j+i)%2==0) {
 
 				// i < 3 means first 3 lines
 				if (i < 3) {
@@ -39,7 +40,9 @@ function generate(x, y) {
 				}
 				// i >x-4 means last 3 lines
 				else if (i > x-4) {
-					checker[i][j] = 2;
+					checker[i][j] = -1;
+				} else {
+					checker[i][j] = 0;
 				}
 			}
 			else {
@@ -48,13 +51,13 @@ function generate(x, y) {
 			}
 
 			var img = '';
-			if (checker[i][j] > 0) {
-				img = '<img class="token" src="'+players[checker[i][j]-1]+'"/>';
+			if (checker[i][j] != 0) {
+				img = '<img class="token" id="i'+j+'-'+i+ '" x="'+i+'" y="'+j+'" src="'+players[0.5-0.5*checker[i][j]]+'"/>';
 			}
 
 			// Get actual color
 			var color = colors[(i+j)%2];
-			$("#checker").append("<div class='"+color+"' id='"+j+","+i+"'>"+img+"</div>");
+			$("#checker").append("<div class='"+color+"' id='"+j+"-"+i+"'>"+img+"</div>");
 		}
 
 		// Jump to next line
@@ -65,7 +68,10 @@ function generate(x, y) {
 	$(".black,.white").droppable({
 		drop:tokenDropped
 	});
-	$(".token").draggable();
+	$(".token").draggable({
+		start: startMovingToken,
+		stop: stopMovingToken
+	});
 
 }
 
@@ -78,7 +84,34 @@ function unselect() {
 }
 
 function tokenDropped() {
-	var x = this.id.split(',');
-	checker[x[0],x[1]] = 5;
-	alert(checker);
+	var x = this.id.split('-');
+
+	// If move is valid
+	if (true) {
+		// checker[x[0],x[1]] = actualPlayer;
+		actualToken.attr("x", x[0]);
+		actualToken.attr("y", x[1]);
+		actualPlayer *= -1;
+	} else {
+		// Undo move
+	}
+}
+
+function startMovingToken() {
+	var x = $(this).attr("x"),
+	    y = $(this).attr("y");
+	actualToken = $(this);
+	highlightValidMoves(validMoves(checker,parseInt(x),parseInt(y)));
+}
+
+function stopMovingToken() {
+}
+
+function highlightValidMoves(list) {
+	// Highlight each posible cell
+	for (var i = 0; i < list.length; i++) {
+		var validMove = list[i];
+		var validId = "#"+validMove[1]+"-"+validMove[0];
+		$(validId).css("background-color", "red");
+	}
 }
